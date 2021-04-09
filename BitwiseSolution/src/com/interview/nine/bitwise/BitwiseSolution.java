6b8b6a2a7ac55607825c138b4a4befe0f3953948
beuhier <shianett@gmail.com>
2021-04-09 21:49:45 +0100

    Update

--- BitwiseSolution/src/com/interview/nine/bitwise/BitwiseSolution.java
+++ BitwiseSolution/src/com/interview/nine/bitwise/BitwiseSolution.java
@@ -2,18 +2,18 @@ package com.interview.nine.bitwise;
 
 import java.util.Arrays;
 
-class Bitwise{
+abstract class Bitwise{
 	
 	public static void main(String[] args){
-		testProgram();
+		//testProgram();
 	}
 	
-	public static void testProgram() {
-		int a [] = {1,2,3,4,5}; int b [] = {11,12,13,14,15};
-		   System.out.println("Original Array A" + Arrays.toString(a));
-		   System.out.println("Original Array B" + Arrays.toString(b));
-		encodeArray(a, b, 5);
-	}
+//	public static void testProgram() {
+//		int a [] = {1,2,3,4,5}; int b [] = {11,12,13,14,15};
+//		   System.out.println("Original Array A" + Arrays.toString(a));
+//		   System.out.println("Original Array B" + Arrays.toString(b));
+//		encodeArray(a, b, 5);
+//	}
 	
 	static int encodeInteger(int x, int n){
 		n = n<<(1<<(1<<(1<<1)));
@@ -21,15 +21,15 @@ class Bitwise{
 		return x;
 	}
 
-	static void encodeArray(int [] A, int [] B, int n){
+	static int [] encodeArray(int [] A, int [] B, int n){
 		for(int i=0;i<n;i++) {
     	    A[i] = encodeInteger(A[i], B[i]);
 		}
-	   System.out.println("Encoded Array A" + Arrays.toString(A));
-		decodeArray(A,n);
+	    //System.out.println("Encoded Array A" + Arrays.toString(A));
+		return decodeArray(A,n);
 	}
 	
-	static void decodeArray(int [] originalArray, int count_of_elements_in_array) {
+	static int[] decodeArray(int [] originalArray, int count_of_elements_in_array) {
 		final int arrayDecoder = 0b00000000000000001111111111111111;
 		int new_a[] = new int[count_of_elements_in_array], new_b[] = new int[count_of_elements_in_array] , a, select;
 	    
@@ -42,7 +42,7 @@ class Bitwise{
 		
 	   System.out.println("New Array A" + Arrays.toString(new_a));
 	   System.out.println("New Array B" + Arrays.toString(new_b));
+	   return new_a;
 	}
-
 	
 }
---
+++ BitwiseSolution/src/com/interview/nine/bitwise/Test/BitwiseSolutionTest.java
@@ -0,0 +1,20 @@
+package com.interview.nine.bitwise.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import com.interview.nine.bitwise.BitwiseSolution;
+
+class BitwiseSolutionTest {
+	BitwiseSolution b;
+	
+	@Test
+	void test() {
+		int a [] = {1,2,3,4,5}; int b [] = {11,12,13,14,15};
+		 Assertions.assertEquals(BitwiseSolution.encodeArray(a, b, 5), 1);
+	}
+
+}
